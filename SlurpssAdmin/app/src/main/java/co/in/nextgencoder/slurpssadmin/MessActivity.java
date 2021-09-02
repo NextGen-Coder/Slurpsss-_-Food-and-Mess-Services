09af871eccdec7e7ae6db3ccf9be5f2b977e3406
Apurwa <apurwaladke1998@gmail.com>
2021-09-02 14:53:19 +0530

    Add Mess

---
+++ SlurpssAdmin/app/src/main/java/co/in/nextgencoder/slurpssadmin/MessActivity.java
@@ -0,0 +1,14 @@
+package co.in.nextgencoder.slurpssadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mess);
+    }
+}
