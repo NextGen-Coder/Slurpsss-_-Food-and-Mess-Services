77591d5eed2db8685f4f46698e6f8c2d25faaa50
Apurwa <apurwaladke1998@gmail.com>
2021-09-03 12:56:02 +0530

    Add Mess activity

--- SlurpssAdmin/app/src/main/java/co/in/nextgencoder/slurpssadmin/MessActivity.java
+++ SlurpssAdmin/app/src/main/java/co/in/nextgencoder/slurpssadmin/MessActivity.java
@@ -2,7 +2,9 @@ package co.in.nextgencoder.slurpssadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MessActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mess);
+    }
+
+    public void addMess(View view) {
+        Intent intent = new Intent(this, AddMessActivity.class);
+        startActivity(intent);
     }
 }
