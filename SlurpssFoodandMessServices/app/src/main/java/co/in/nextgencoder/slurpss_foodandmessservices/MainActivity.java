e708020a40dbb184eee9a9fe6184e25acb2e44f9
sanjana-dot <sanjanavaidya44@gmail.com>
2021-09-11 14:09:49 +0530

    mess login activity

--- SlurpssFoodandMessServices/app/src/main/java/co/in/nextgencoder/slurpss_foodandmessservices/MainActivity.java
+++ SlurpssFoodandMessServices/app/src/main/java/co/in/nextgencoder/slurpss_foodandmessservices/MainActivity.java
@@ -2,6 +2,7 @@ package co.in.nextgencoder.slurpss_foodandmessservices;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gotoMess(View view) {
+        Intent intent = new Intent(MainActivity.this, MessLoginActivity.class);
+        startActivity(intent);
     }
 }
