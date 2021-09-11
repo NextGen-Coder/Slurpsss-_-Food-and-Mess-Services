0f5f723e280d6e9a85369310720df803a1b423a2
prajakta-hue <satikosarep9@gmail.com>
2021-09-11 14:15:06 +0530

    reset password

---
+++ SlurpssFoodandMessServices/app/src/main/java/co/in/nextgencoder/slurpss_foodandmessservices/MessResetPassword.java
@@ -0,0 +1,18 @@
+package co.in.nextgencoder.slurpss_foodandmessservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MessResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mess_reset_password);
+    }
+
+    public void Submit(View view) {
+    }
+}
