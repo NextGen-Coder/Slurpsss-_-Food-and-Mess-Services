4602706cd331e8f0b2b9a834af26a026cc279d82
prajakta-hue <satikosarep9@gmail.com>
2021-09-11 12:51:49 +0530

    Added burger in App

--- SlurpssFoodandMessServices/app/src/main/java/co/in/nextgencoder/slurpss_foodandmessservices/UserActivity.java
+++ SlurpssFoodandMessServices/app/src/main/java/co/in/nextgencoder/slurpss_foodandmessservices/UserActivity.java
@@ -3,19 +3,12 @@ package co.in.nextgencoder.slurpss_foodandmessservices;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void gotoUser(View view) {
-    }
-
-    public void gotoMess(View view) {
+        setContentView(R.layout.activity_user);
     }
 }
