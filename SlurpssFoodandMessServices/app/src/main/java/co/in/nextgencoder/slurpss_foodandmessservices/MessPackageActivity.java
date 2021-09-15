f91a73daa2276835acf1d25ff862d80ae7cad3f6
Apurwa <apurwaladke1998@gmail.com>
2021-09-15 12:42:52 +0530

    Mess package activity

---
+++ SlurpssFoodandMessServices/app/src/main/java/co/in/nextgencoder/slurpss_foodandmessservices/MessPackageActivity.java
@@ -0,0 +1,14 @@
+package co.in.nextgencoder.slurpss_foodandmessservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MessPackageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mess_package);
+    }
+}
