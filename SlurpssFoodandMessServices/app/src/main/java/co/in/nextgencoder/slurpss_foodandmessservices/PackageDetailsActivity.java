92e4f7378f5b245911df1aa5e93ffb11e44af6ad
Apurwa <apurwaladke1998@gmail.com>
2021-10-23 13:19:39 +0530

    Item Package Design

--- SlurpssFoodandMessServices/app/src/main/java/co/in/nextgencoder/slurpss_foodandmessservices/PackageDetailsActivity.java
+++ SlurpssFoodandMessServices/app/src/main/java/co/in/nextgencoder/slurpss_foodandmessservices/PackageDetailsActivity.java
@@ -3,6 +3,7 @@ package co.in.nextgencoder.slurpss_foodandmessservices;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class PackageDetailsActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class PackageDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_package_details);
+    }
+
+    public void submitDish(View view) {
+
     }
 }
