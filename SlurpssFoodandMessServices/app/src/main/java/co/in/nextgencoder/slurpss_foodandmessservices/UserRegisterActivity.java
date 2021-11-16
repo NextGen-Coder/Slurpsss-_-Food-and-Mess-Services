e534b46de2287fde681741d51cbe1b78914b2ca8
sanjana-dot <sanjanavaidya44@gmail.com>
2021-11-16 13:01:25 +0530

    list and delete dish backend
    # Conflicts:
    #	SlurpssFoodandMessServices/app/src/main/res/layout/item_mess_dish.xml
    #	SlurpssFoodandMessServices/app/src/main/res/values/preloaded_fonts.xml

---
+++ SlurpssFoodandMessServices/app/src/main/java/co/in/nextgencoder/slurpss_foodandmessservices/UserRegisterActivity.java
@@ -0,0 +1,21 @@
+package co.in.nextgencoder.slurpss_foodandmessservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class UserRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+    }
+
+    public void submitUserRegister(View view) {
+    }
+
+    public void userLogin(View view) {
+    }
+}
